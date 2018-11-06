ed3eb10f4c5bad89281e7525b07d3405ba1cf6e7
Scoon90 <43672342+Scoon90@users.noreply.github.com>
2018-11-06 10:01:07 +0400

    Update project.js

--- project.js
+++ project.js
@@ -1,39 +1,32 @@
 function makeGride(){
-  console.log("makeGrid is running!");
-  var table = document.getElementById("pixelCanvas");
-  var row = document.createElement("tr");
-  var thight = document.getElementById("inputHeight").value;
-  var twidth = document.getElementById("inputWidth").value;
-  CellH = document.createElement("td");
+  var table = document.getElementById('pixelCanvas');
+  var row = document.createElement('tr');
+  var createhight = document.getElementById('inputHeight').value;
+  var createwidth = document.getElementById('inputWidth').value;
+ var cloume = document.createElement('td');
 
   table.innerHTML = '';
+  
 
- 
-      table.appendChild(row)
-
-    for (let x = 0; x < thight; x++) {
-      
-    for (let y = 0; y < twidth; y++) {
-      row.appendChild(CellH);
-      //alert("W"+y)
+  for (let x = 1; x <= createhight; x++) {
+     row; 
+    for (let y = 1; y <= createwidth; y++){
+      cloume;
+      row.appendchiled(cloume);
     }
     alert("H"+x)
 
 
     }
-   
+   table.appendChild(row)
   }
 
   document.addEventListener("submit", tValue);
   function tValue(event){
   event.preventDefault()
   makeGride();
-  clearGride();
   }
 
-function clearGride(){
-  document.remove (table);
-}
 
 var color =document.getElementById('colorPicker');
 color.querySelector('color');
