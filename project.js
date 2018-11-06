59e5eca2ea6475fee7a26c0f8771726490919b1c
Scoon90 <43672342+Scoon90@users.noreply.github.com>
2018-11-06 14:50:24 +0400

    Update project.js

--- project.js
+++ project.js
@@ -1,35 +1,33 @@
-function makeGride(){
-  var table = document.getElementById('pixelCanvas');
-  var row = document.createElement('tr');
-  var createhight = document.getElementById('inputHeight').value;
-  var createwidth = document.getElementById('inputWidth').value;
- var cloume = document.createElement('td');
+//declear Variables
+var drawtable = document.getElementById("pixelCanvas");
+var size = document.getElementById("sizePicker");
+// add event listener to submite butome
+size.addEventListener("submit", tvalue);
+function tvalue(event){
+event.preventDefault();
 
-  table.innerHTML = '';
-  
+makeGrid();
 
-  for (let x = 1; x <= createhight; x++) {
-     row; 
-    for (let y = 1; y <= createwidth; y++){
-      cloume;
-      row.appendchiled(cloume);
-    }
-    alert("H"+x)
+}
+// this function will be used to create table and select color
+function makeGrid(){
 
-
-    }
-   table.appendChild(row)
+ var getwidth = document.getElementById("inputWidth").value;
+ var getheight = document.getElementById("inputHeight").value;
+ //to clear the inner value when there is 
+ drawtable.innerHTML = '';
+ // loop to create raws and colums
+ for (var i =1 ; i<= getheight ; i++){
+   var row = document.createElement("tr");
+   for (var y =1 ; y<= getwidth ; y++){
+     var colume = document.createElement("td");
+	 row.appendChild(colume);
+	 colume.addEventListener("click", selectcolor);
+        function selectcolor(event){
+        //event.preventDefault();
+        var color = document.getElementById("colorPicker").value;
+        colume.style.background = color;})
+	}
+	drawtable.appendChild(row);
   }
-
-  document.addEventListener("submit", tValue);
-  function tValue(event){
-  event.preventDefault()
-  makeGride();
-  }
-
-
-var color =document.getElementById('colorPicker');
-color.querySelector('color');
-function getColor(colorPicker){
-   color= document.getElementById('colorPicker').value;
 }
